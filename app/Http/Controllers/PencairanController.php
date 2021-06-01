b03d9792c308714fc64525b78f3bd98b45f88c7c
farhan048 <achmadfarhanmushafa@gmail.com>
2021-06-01 11:43:54 +0700

    PencairanController update

--- app/Http/Controllers/PencairanController.php
+++ app/Http/Controllers/PencairanController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Pencairan;
+use App\Pembayaran;
 use Illuminate\Http\Request;
 
 class PencairanController extends Controller
@@ -10,8 +11,18 @@ class PencairanController extends Controller
     
     public function index()
     {
+        $datas = Pencairan::all();
        return view ('admin.saldo');
     }
 
+    public function store(Request $request)
+    {
+        # code...
+    }
+
+    public function update()
+    {
+        # code...
+    }
   
 }
