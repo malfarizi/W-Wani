f338dc760415e1e197cddb26c7b111b82aabb285
Firman Syah <abdulfirmansyah7@gmail.com>
2021-06-09 10:52:18 +0700

    clean

--- app/Http/Controllers/DummyController.php
+++ app/Http/Controllers/DummyController.php
@@ -2,45 +2,84 @@
 
 namespace App\Http\Controllers;
 
-use App\Mitra;
-use App\Produk;
-use App\Alamat;
-use App\Kategori;
+use DB;
 use Illuminate\Http\Request;
 
 class DummyController extends Controller
 {
     public function test(){
-        $data_alamat = ['id_kota' => 149, 'alamat_lengkap' => 'jatibarang'];
-        $alamat = Alamat::create($data_alamat);
 
-        $data_mitra = [
-            'nama_mitra' => 'Farhan',
-            'email' => 'farhan@gmail.com',
-            'password' => bcrypt('farhan'), 
+        DB::table('alamat')->insert([
+            'id_alamat' => 1, 
+            'id_kota' => 17, 
+            'alamat_lengkap' => 'Sukajadi',
+        ]);
+
+        DB::table('alamat')->insert([
+            'id_alamat' => 2, 
+            'id_kota' => 17, 
+            'alamat_lengkap' => 'Lembang',
+        ]);
+
+        DB::table('kategori')->insert([
+        	'id_kategori' => 1,
+        	'nama_kategori' => 'Beras',
+        ]);
+
+        DB::table('mitra')->insert([
+            'id_mitra' => 1,
+            'nama_mitra' => 'Petani',
+            'email' => 'petani123@gmail.com',
+            'password' => 'petani123', 
             'jk' => 'laki-laki',
-            'no_telp' => '089',
+            'no_telp' => '0823648221',
             'foto' => '',
-            'status' => 'ok',
-            'level' => 'mitra',
-            'no_rek' => '0333',
-            'nama_rekening' => 'Farhan',
-            'nama_bank' => 'BNI',
-            'id_alamat' => $alamat->id_alamat
-        ];
-        $mitra = Mitra::create($data_mitra);
-        $kategori = Kategori::create(['nama_kategori' => 'buah']);
-        $data_produk = [
-            'nama_produk' => 'jeruk',
-            'deskripsi' => 'segar dan manis',
-            'id_kategori' => $kategori->id_kategori, 
-            'satuan' => 'gram',
-            'harga' => '1000',
-            'qty' => '30',
-            'berat' => '6',
-            'foto' => 'jeruk.jpg',
-            'id_mitra' => $mitra->id_mitra
-        ];
-        Produk::create($data_produk);
+            'status' => 'Diterima',
+            'level' => 'Petani',
+            'no_rek' => '325235324',
+            'nama_rekening' => 'Petani',
+            'nama_bank' => 'BRI',
+            'id_alamat' => 1
+        ]);
+
+        DB::table('mitra')->insert([
+            'id_mitra' => 2,
+            'nama_mitra' => 'Vendor',
+            'email' => 'vendor123@gmail.com',
+            'password' => 'petani', 
+            'jk' => 'laki-laki',
+            'no_telp' => '0823648221',
+            'foto' => '',
+            'status' => 'Diterima',
+            'level' => 'Vendor',
+            'no_rek' => '325235324',
+            'nama_rekening' => 'Vendor',
+            'nama_bank' => 'BRI',
+            'id_alamat' => 2
+        ]);
+
+        DB::table('alat')->insert([
+            'id_alat' => 1,
+            'status' => 'Tersedia',
+            'desc' => 'Perontok Padi',
+            'foto' => '1622657644_perontok padi.jpg', 
+            'harga' => 15000,
+            'nama_alat' => 'Traktor',
+            'kategori', => 'Traktor',
+            'id_mitra' => 2
+        ]);
+
+        DB::table('produk')->insert([
+        	'id_produk' => 1,
+        	'nama_produk' => 'Beras',
+        	'deskripsi' => 'Beras putih murah dan berkualitas',
+        	'qty' => 10,
+            'harga' => 10000,
+            'satuan' => 'kg',
+            'berat' => 500,
+            'foto' => '',
+            'id_mitra' => 1,
+            'id_kategori' => 1,
+        ]);
     }
 }
